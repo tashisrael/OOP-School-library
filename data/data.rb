7f43c9f3a4f44290c2fac6f8c341bd5cba3ce1aa
tashisrael <tashisrael04@gmail.com>
2023-01-11 11:50:59 +0300

    Read persons from JSON

--- data/data.rb
+++ data/data.rb
@@ -1,3 +1,19 @@
+def read_person
+  if File.exist?('./data/people.json')
+
+    persons = JSON.parse(File.read('./data/people.json'))
+    persons.map do |person|
+      if person['occupation'] == 'Teacher'
+        Teacher.new(person['name'], person['age'], person['specialization'])
+      else
+        Student.new(person['name'], person['age'], person['parent_permission'])
+      end
+    end
+  else
+    []
+    end
+end
+
 def save_persons
   person_store = @persons.map do |person, _index, _id|
     if person.instance_of?(Teacher)
@@ -18,5 +34,29 @@ def save_persons
   end
   File.open('./data/people.json', 'w') do |file|
     file.puts(JSON.pretty_generate(person_store))
+  end
+end
+
+def save_books
+  file = File.open('./data/book.json', 'w')
+  book_store = @books.map do |book|
+    { title: book.title, author: book.author }
+  end
+  file.puts(JSON.pretty_generate(book_store))
+end
+
+def save_rentals(date, person_id, book_id)
+  new_data = []
+  if File.exist?('./data/rentals.json')
+    rents = JSON.parse(File.read('./data/rentals.json'))
+    rents.map do |rent|
+      new_data.push(rent)
+    end
+  else
+    []
+  end
+  new_data.push({ Date: date, book_index: book_id, person_index: person_id })
+  File.open('./data/rentals.json', 'w') do |file|
+    file.puts(JSON.pretty_generate(new_data))
   end
 end
