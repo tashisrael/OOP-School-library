d78d8e418fb0cbdb7dc02f7301347772688b7cb3
Irirwanirira <irijoseph41@gmail.com>
2023-01-11 16:32:48 +0200

    Add classroom test

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../classroom'
+
+describe Classroom do
+    context 'When testing the Classroom class' do
+      classes = Classroom.new('classroom')
+  
+      it 'should display Classroom ' do
+        expect(classes).to be_an_instance_of Classroom
+      end
+      it 'classes has to be classroom' do
+        expect(classes.label).to eq 'classroom'
+      end
+    end
+end
