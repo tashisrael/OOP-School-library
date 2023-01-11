c2e5eb8c1d2c43207cc17fdfa5e70ecbb326350a
Irirwanirira <irijoseph41@gmail.com>
2023-01-11 16:51:18 +0200

    Create rental test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,20 +1,20 @@
 require_relative '../teacher'
 
 describe Teacher do
-    context 'When testing teacher class' do
-      teacher = Teacher.new('50', 'Footbal', 'Pep')
-  
-      it 'should display age, specialization and name ' do
-        expect(teacher).to be_an_instance_of Teacher
-      end
-      it 'Teacher name ' do
-        expect(teacher.name).to eq 'Pep'
-      end
-      it 'teacher age' do
-        expect(teacher.age).to eq '50'
-      end
-      it 'Teacher parent specializatio' do
-        expect(teacher.specialization).to eq 'Footbal'
-      end
+  context 'When testing teacher class' do
+    teacher = Teacher.new('50', 'Footbal', 'Pep')
+
+    it 'should display age, specialization and name ' do
+      expect(teacher).to be_an_instance_of Teacher
     end
+    it 'Teacher name ' do
+      expect(teacher.name).to eq 'Pep'
+    end
+    it 'teacher age' do
+      expect(teacher.age).to eq '50'
+    end
+    it 'Teacher parent specializatio' do
+      expect(teacher.specialization).to eq 'Footbal'
+    end
+  end
 end
